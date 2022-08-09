8ca211c36c0c5fb786c3e01f139ef151e4a3cbde
Arli Machado <arli.machado@gmail.com>
2022-08-09 12:33:06 -0300

    Definindo componente Header

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from '../../components/Header'
+import * as S from './styles'
+
+export function Transactions() {
+  return (
+    <S.Container>
+      <Header />
+    </S.Container>
+  )
+}
