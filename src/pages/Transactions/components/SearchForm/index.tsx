ebd6526a33c1942dde0215f9e5d9ff6801f04ff9
Arli Machado <arli.machado@gmail.com>
2022-08-09 23:12:59 -0300

    Configurando react hook form

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,12 +1,38 @@
 import { MagnifyingGlass } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import * as S from './styles'
 
-export function SearchForm() {
-  return (
-    <S.Container>
-      <input type="text" placeholder="Busque por transações" />
+const searchFormSchema = z.object({
+  query: z.string(),
+})
 
-      <button type="submit">
+type SearchFormInputs = z.infer<typeof searchFormSchema>
+
+export function SearchForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  function handleSearchtransactions({ query }: SearchFormInputs) {
+    console.log(query)
+  }
+
+  return (
+    <S.Container onSubmit={handleSubmit(handleSearchtransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>
--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -32,8 +32,14 @@ export const Container = styled.form`
       color: ${theme['green-300']};
       font-weight: bold;
       border-radius: 6px;
+      cursor: pointer;
 
-      &:hover {
+      &:disabled {
+        opacity: 0.6;
+        cursor: not-allowed;
+      }
+
+      &:not(:disabled):hover {
         background: ${theme['green-500']};
         border-color: ${theme['green-500']};
         color: ${theme.white};
